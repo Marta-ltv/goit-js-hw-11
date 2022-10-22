b987a8dbd1da190468a7dafcb67870fee58f8b4e
Marta Litvinchuk <103766196+Marta-ltv@users.noreply.github.com>
2022-10-23 01:32:31 +0300

    Initial commit

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,16 @@
+export default class FetchApiService  {
+    constructor() { 
+        this.searchCountry = '';
+    }
+    
+    fetchCountries() {
+    const url = `https://restcountries.com/v3.1/name/${this.searchCountry}?fields=name,capital,languages,population,flags`;
+    return fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+}
+
