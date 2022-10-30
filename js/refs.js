c1b10107ca31f2064328053ca56861ed59f985ad
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-10-30 18:05:34 +0200

    delete file

--- js/refs.js
+++
@@ -1,6 +0,0 @@
- export default refs = {
-    formRef: document.querySelector('.search-form'),
-    inputRef: document.querySelector('.input-search'),
-    searchBtn: document.querySelector('.btn-search'),
-    loadMoreBtn: document.querySelector('.load-more'),
-}
